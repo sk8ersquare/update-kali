fd2844ba485cfa87993c227d73654c66a46a9d13
sk8ersquare <sk8er_square@hotmail.com>
2023-09-18 19:38:35 +1000

    Update config.py

--- config.py
+++ config.py
@@ -16,18 +16,23 @@ if 'Kali' in release:
     directories_to_remove = ['Documents', 'Music', 'Pictures', 'Public', 'Templates', 'Videos']
 
     # These kali packages will be installed
-    packages_to_install = ['most', 'ttf-mscorefonts-installer', 'pydf', 'htop', 'gobuster', 'amass',
-                           'golang', 'exif', 'hexedit', 'jq', 'python3-pip', 'python3-venv',
-                           'apt-transport-https', 'curl', 'filezilla', 'meld', 'ncat', 'net-tools',
-                           'tmux', 'bash-completion', 'ieee-data', 'python3-netaddr',
-                           'ruby-full', 'powercat', 'cewl', 'nbtscan', 'tree', 'upx-ucl',
-                           'exe2hexbat', 'shellter', 'grc', 'tor', 'torbrowser-launcher']
+    packages_to_install = ['most', 'ttf-mscorefonts-installer', 'gobuster', 'amass',
+                           'golang', 'exif', 'hexedit', 'jq', 'curl', 'filezilla', 'net-tools',
+                           'tmux', 'bash-completion', 'ieee-data', 'powercat', 'cewl', 'nbtscan', 
+                           'tree', 'shellter', 'grc', 'passing-the-hash', 'evil-winrm', 'mimikatz', 
+                           'impacket', 'feroxbuster', 'ffuf', 'freerdp2', 'bloodhound', 'hashdeep',
+                           'crackmapexec', 'creddump7', 'hping3', 'dos2unix', 'dotdotpwn', 'impacket-scripts',
+                           'nishang', 'onesixtyone', 'kerberoast', 'patator', 'peass-ng', 'powersploit',
+                           'powershell-empire', 'pwncat', 'python-pip', 'python-virtualenv', 'rdesktop',
+                           'seclists', 'secure-socket-funneling-windows-binaries', 'sherlock', 'smbmap',
+                           'snmpcheck', 'windows-privesc-check', 'rlwrap', 'whatweb']
 
     # These kali packages will be removed
     packages_to_remove = []
 
     # These python packages will be installed globally
-    pip_packages = ['pipenv', 'pylint', 'dnsgen', 'stegcracker', 'truffleHog', 'apkleaks', 'defaultcreds-cheat-sheet']
+    pip_packages = ['pipenv', 'pylint', 'defaultcreds-cheat-sheet', 'wesng', 'certipy-ad', 'pycryptodomex', 'gssapi', 'ldap3', 'pywerview', 'terminator', 
+                    'pyperclip3', 'updog', 'pwnshelter', 'colorama', 'readchar', 'netifaces']
 
     # These gem packages will be installed globally
     gem_packages = ['wpscan']
@@ -41,24 +46,76 @@ if 'Kali' in release:
     golang_install_directory = '/opt'
     golang_modules_to_install = [
                                 'github.com/lc/gau',
-                                'github.com/hakluke/hakrawler',
-                                'github.com/hahwul/dalfox',
-                                'github.com/projectdiscovery/nuclei/v2/cmd/nuclei',
-                                'github.com/Shopify/kubeaudit',
-                                'github.com/tomnomnom/httprobe'
+                                'github.com/ropnop/go-windapsearch',
+                                'github.com/ropnop/kerbrute',
+                                'github.com/CristinaSolana/ggtfobins'
                                 ]
 
     # These git repositories will be synced to the 'external repo' directory
     external_tools_directory = '/opt'
     ext_repositories_to_sync =  [
                                 'https://github.com/swisskyrepo/PayloadsAllTheThings',
+                                'https://github.com/Hacking-Notes/VulnScan',
+                                'https://github.com/p0dalirius/LDAPmonitor',
+                                'https://github.com/GhostPack/Certify',
+                                'https://github.com/GhostPack/SafetyKatz',
+                                'https://github.com/GhostPack/Seatbelt',
+                                'https://github.com/GhostPack/Rubeus',
+                                'https://github.com/SnaffCon/Snaffler/releases/download/1.0.132/Snaffler.exe',
+                                'https://github.com/p0dalirius/FindUncommonShares',
+                                'https://github.com/ShutdownRepo/targetedKerberoast',
+                                'https://github.com/shutdownrepo/pywhisker',
+                                'https://github.com/topotam/PetitPotam',
+                                'https://github.com/dirkjanm/PKINITtools',
+                                'https://github.com/TH3xACE/SUDO_KILLER',
+                                'https://github.com/cwinfosec/revshellgen',
+                                'https://github.com/Tib3rius/windowsprivchecker',
+                                'https://github.com/Tib3rius/creddump7',
+                                'https://github.com/Tib3rius/Windows-PrivEsc-Tools',
+                                'https://github.com/Tib3rius/Linux-PrivEsc-Tools',
+                                'https://github.com/61106960/adPEAS',
+                                'https://github.com/nicocha30/ligolo-ng/releases/download/v0.4.4/ligolo-ng_agent_0.4.4_windows_amd64.zip',
+                                'https://github.com/nicocha30/ligolo-ng/releases/download/v0.4.4/ligolo-ng_agent_0.4.4_linux_amd64.tar.gz',
+                                'https://github.com/cddmp/enum4linux-ng',
+                                'https://github.com/Anon-Exploiter/SUID3NUM',
+                                'https://github.com/The-Z-Labs/linux-exploit-suggester',
+                                'https://github.com/DominicBreuker/pspy/releases/download/v1.2.1/pspy32',
+                                'https://github.com/DominicBreuker/pspy/releases/download/v1.2.1/pspy64',
+                                'https://github.com/DominicBreuker/pspy/releases/download/v1.2.1/pspy32s',
+                                'https://github.com/DominicBreuker/pspy/releases/download/v1.2.1/pspy64s',
+                                'https://github.com/faustinoaq/sswws/releases/download/v0.1.0/sswws.zip',
+                                'https://github.com/RustScan/RustScan/archive/refs/tags/2.1.1.zip',
+                                'https://github.com/Tib3rius/AutoRecon',
+                                'https://github.com/Tib3rius/SemiAutoRecon',
+                                'https://github.com/CMEPW/keepass-dump-masterkey',
+                                'https://github.com/Orange-Cyberdefense/KeePwn',
+                                'https://github.com/AlessandroZ/LaZagne/releases/download/v2.4.5/LaZagne.exe',
+                                'https://github.com/r3motecontrol/Ghostpack-CompiledBinaries/',
+                                'https://github.com/n00py/LAPSDumper',
+                                'https://github.com/Hackndo/lsassy',
+                                'https://github.com/S3cur3Th1sSh1t/PowerSharpPack',
+                                'https://github.com/PowerShellMafia/PowerSploit/blob/master/Privesc/PowerUp.ps1',
+                                'https://github.com/PowerShellMafia/PowerSploit/blob/master/Recon/PowerView.ps1',
+                                'https://github.com/aniqfakhrul/powerview.py',
+                                'https://github.com/gtworek/Priv2Admin',
+                                'https://github.com/byt3bl33d3r/pth-toolkit',
+                                'https://github.com/antonioCoco/RunasCs',
+                                'https://github.com/OPENCYBER-FR/RustHound',
+                                'https://github.com/SpiderLabs/scavenger',
+                                'https://github.com/eladshamir/Whisker',
+                                'https://github.com/blankshiro/OSCP-Tools',
+                                'https://github.com/Syslifters/offsec-tools',
+                                'https://github.com/mikes-hacks/mxhelp',
+                                'https://github.com/OlivierLaflamme/Cheatsheet-God',
+                                'https://github.com/CoolHandSquid/TireFire/tree/TireFire_V4',
+                                'https://github.com/antonioCoco/ConPtyShell',
+                                'https://github.com/wirzka/incursore',
+                                'https://github.com/ropnop/impacket_static_binaries/releases/tag/0.9.22.dev-binaries'  
                                 ]
 
     # These git repositories will be synced to the 'personal repo' directory. I use my home directory.
     personal_repo_directory = os.getenv("HOME")
     personal_repositories_to_sync = [
-                                    'git@github.com:rafaelh/dotfiles',
-                                    'git@github.com:rafaelh/.private'
                                     ]
 
     # Next, take a look in the /scripts directory. Every script ending in .sh or .py will be run,
